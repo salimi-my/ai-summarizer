c92533eef992327b9a691bafe20c887427cf87a3
Mohamad Salimi <contact@salimi.my>
2023-06-24 02:50:31 +0800

    Add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Hero from './components/Hero';
 import Demo from './components/Demo';
 import './App.css';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
@@ -9,10 +10,12 @@ const App = () => {
         <div className='gradient' />
       </div>
 
-      <div className='app'>
+      <div className='app min-h-[calc(100vh_-_32px)]'>
         <Hero />
         <Demo />
       </div>
+
+      <Footer />
     </main>
   );
 };
---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+  return (
+    <footer className='text-white relative z-10 text-center pb-2'>
+      <small>
+        Created by{' '}
+        <a
+          href='https://www.salimi.my'
+          target='_blank'
+          rel='noreferrer'
+          className='hover:underline'
+        >
+          Salimi
+        </a>
+      </small>
+    </footer>
+  );
+};
+
+export default Footer;
