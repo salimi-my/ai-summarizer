3869d9c2481d3fe3228a287ddca69c79f08a2380
Mohamad Salimi <contact@salimi.my>
2023-06-23 23:26:41 +0800

    Save articles to local storage

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -10,6 +10,18 @@ const Demo = () => {
 
   const [allArticles, setAllArticles] = useState([]);
 
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+  useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(
+      localStorage.getItem('articles')
+    );
+
+    if (articlesFromLocalStorage) {
+      setAllArticles(articlesFromLocalStorage);
+    }
+  }, []);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -18,9 +30,12 @@ const Demo = () => {
     if (data?.summary) {
       const newArticle = { ...article, summary: data.summary };
 
-      setArticle(newArticle);
+      const updatedAllArticles = [newArticle, ...allArticles];
 
-      console.log(newArticle);
+      setArticle(newArticle);
+      setAllArticles(updatedAllArticles);
+
+      localStorage.setItem('articles', JSON.stringify(updatedAllArticles));
     }
   };
 
